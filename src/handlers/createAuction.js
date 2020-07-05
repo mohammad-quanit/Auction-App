953a5bd96771daddb826f63fa5b8f7faceea2776
mohammad-quanit <muhammadquanit@gmail.com>
2020-07-06 01:02:03 +0500

    add schema based validation on create and get aunctions

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,11 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
 import createError from "http-errors";
+import validator from "@middy/validator";
 
 import commonMiddleware from "../lib/commonMiddleware";
+import createAuctionSchema from "../lib/schemas/createAuctionSchema";
+
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -41,4 +44,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(validator({ inputSchema: createAuctionSchema }));
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,15 +1,31 @@
 import AWS from "aws-sdk";
 import createError from "http-errors";
+import validator from "@middy/validator";
 
 import commonMiddleware from "../lib/commonMiddleware";
+import getAuctionSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ":status": status
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    }
+  }
+
   try {
-    const result = await dynamodb.scan({ TableName: process.env.DYNAMODB_TABLE }).promise();
+    // const result = await dynamodb.scan({ TableName: process.env.DYNAMODB_TABLE }).promise();
+    const result = await dynamodb.query(params).promise();
     auctions = result.Items;
   } catch (error) {
     console.error(error);
@@ -22,4 +38,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(validator({ inputSchema: getAuctionSchema, useDefaults: true }));
